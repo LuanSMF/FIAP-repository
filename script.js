4a0d77c13653039ec002378e85acb6545bb092de
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-30 11:11:44 -0300

    adicionando aula03 de Web

---
+++ script.js
@@ -0,0 +1,20 @@
+let numero = prompt('Digite um numero:');
+let numeros = prompt('Digite um numero:');
+
+let soma = parseFloat(numero)+parseFloat(numeros);
+let produto = parseFloat(numero)*parseFloat(numeros);
+console.log(`${numero} + ${numeros} = ${soma}`);
+alert(`${numero} * ${numeros} = ${produto}`)
+
+/*
+console.log('Bem-vindo!');
+console.log("Olá, " +  nome + "!");
+console.log(`Olá ${nome}`);
+
+/*if(nome!=null){
+ let nomeUsuario = document.getElementById('usuario');
+ nomeUsuario.innerText = `Olá, ${nome}`;
+}else{
+alert('Digite algo!')
+}
+*/
